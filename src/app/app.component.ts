3b64c1d32ab53b672ed09595f9c0474262f22caa
unknown <Md. Mamun Hossain>
2024-08-10 00:08:19 +0600

    first commit rotasheet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { YouTubePlayerModule } from "@angular/youtube-player";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, YouTubePlayerModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
